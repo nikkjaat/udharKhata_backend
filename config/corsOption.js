7ef4b09eb66498e808b70c565d656f312707b0ea
Nikhil <nikhiljaat330@gmail.com>
2024-05-09 12:25:47 +0530

    changes

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://udhar-khata-frontend-9axf.vercel.app/login",
+  "https://udhar-khata-frontend-9axf.vercel.app",
 ];
 
 const corsOptions = {
