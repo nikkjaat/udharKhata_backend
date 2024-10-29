0d50c0210fb3b59260d63c19ce326ca880202879
nikkjaat <160124916+nikkjaat@users.noreply.github.com>
2024-10-29 16:05:20 +0530

    Update corsOption.js

--- config/corsOption.js
+++ config/corsOption.js
@@ -20,3 +20,25 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+
+
+
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://udhar-khata-frontend.vercel.app",
+//   "https://udharkhata.netlify.app",
+//   "https://udhaarkhatafrontend.netlify.app"
+// ];
+
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed to access"));
+//     }
+//   },
+//   credentials: true, // Allow credentials
+//   optionsSuccessStatus: 200,
+// }
