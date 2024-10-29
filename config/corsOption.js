58e42a1192099ad090cbc6b587b8705d076f858a
nikkjaat <160124916+nikkjaat@users.noreply.github.com>
2024-10-29 16:19:19 +0530

    Update corsOption.js

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,25 +1,41 @@
-
-
 const allowedOrigins = [
-  "http://localhost:3000",
-  "https://udhar-khata-frontend.vercel.app",
-  "https://udharkhata.netlify.app",
-  "https://udhaarkhatafrontend.netlify.app"
+  "https://udhaarkhatafrontend.netlify.app/",
 ];
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, origin); // Use the exact origin
+      callback(null, true);
     } else {
-      callback(new Error("Not allowed to access"));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true, // Allow credentials
+  credentials: true,
   optionsSuccessStatus: 200,
 };
 
 module.exports = corsOptions;
+
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://udhar-khata-frontend.vercel.app",
+//   "https://udharkhata.netlify.app",
+//   "https://udhaarkhatafrontend.netlify.app"
+// ];
+
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, origin); // Use the exact origin
+//     } else {
+//       callback(new Error("Not allowed to access"));
+//     }
+//   },
+//   credentials: true, // Allow credentials
+//   optionsSuccessStatus: 200,
+// };
+
+// module.exports = corsOptions;
 
 
 
